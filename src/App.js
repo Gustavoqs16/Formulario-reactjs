798b7546c1cde20bb69cb110963a8a838d4fc170
gustavo_qs16@hotmail.com <gustavo_qs16@hotmail.com>
2021-12-07 00:42:01 -0300

    Subindo Portfolio para o GitHub

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Header } from './components/Header/index'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export const Header = () => (
+  <div className="header">
+    <a href="#default" className="personalName">
+      Gustavo Queir&oacute;z de Souza
+    </a>
+    <div className="header-right">
+      <a href="#contact">Contato</a>
+    </div>
+  </div>
+);
